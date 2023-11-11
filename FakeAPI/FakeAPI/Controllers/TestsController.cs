83799c5847533a5e556b02e7b0e846694832245f
Nikola Podrascanin <61381098+npodrascanin9@users.noreply.github.com>
2023-11-11 16:39:18 +0100

    fakes are facts

---
+++ FakeAPI/FakeAPI/Controllers/TestsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeAPI.Controllers
+{
+    [Route("api/tests")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(value: new List<object>()
+            {
+                new { Id = 1, Name = "I'm a fake "},
+                new { Id = 2, Name = "Yeah, and I'm a fake 2!" },
+                new { Id = 3, Name = "Fakes are ment for testing... Better known as: KISS principle"}
+            });
+        }
+    }
+}
